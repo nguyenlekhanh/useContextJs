2eb2e62085ecec1b72debadf6531a0b57caa8c9c
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2022-10-21 22:25:59 -0500

    UserContextProvider

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,3 +1,17 @@
-import { createContext } from "react";
+import { createContext, useMemo, useState } from "react";
 
-export const UserContext = createContext("abc");
+export const UserContext = createContext(null);
+
+const UserContextProvider = (props) => {
+  const [user, setUser] = useState(null);
+
+  const providerValue = useMemo(() => ({ user, setUser }), [user, setUser]);
+
+  return (
+    <UserContext.Provider value={providerValue}>
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContextProvider;
